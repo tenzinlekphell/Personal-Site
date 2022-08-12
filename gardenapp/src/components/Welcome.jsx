f55fe5b1ea93dbee2f11516bb3153d1af2ae0e0e
Tenzin Lekphell <61218333+tenzinlekphell@users.noreply.github.com>
2022-08-12 12:18:37 -0400

    Inserted lorem ipsum content into Welcome.jsx component

---
+++ gardenapp/src/components/Welcome.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../css/WelcomePage.css';
+
+const Welcome = () => {
+  return (
+    <div id="welcome-page">
+      <p>Welcome to my portfolio! </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi
+        pariatur esse earum provident voluptatibus tempore inventore animi
+        sapiente officiis, dignissimos eligendi ea, ex illum eos. Ea voluptate
+        nihil dolorum quibusdam dolorem et itaque natus molestias vero
+        perferendis impedit quidem excepturi nam tempore consequuntur, quis
+        exercitationem error quos fugiat corporis unde voluptas, dignissimos
+        quas ut? Quae, adipisci atque amet delectus ea commodi maxime enim velit
+        qui, perspiciatis quibusdam sed iure facilis autem. Architecto
+        repudiandae dicta accusantium cum ad! Explicabo nesciunt esse dolores
+        provident quisquam non sapiente ipsam cupiditate! Vel sint, quasi fuga
+        aut assumenda deleniti officia ullam tempora mollitia, laborum modi?
+      </p>
+    </div>
+  );
+};
+
+export default Welcome;
